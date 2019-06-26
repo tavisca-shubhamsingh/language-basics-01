b2a3af23eb05e930c2de4f84d11305f42a3cef4d
tavisca-shubhamsingh <ssingh@tavisca.in>
2019-06-26 16:07:11 +0530

    Completed assignment 1

--- Tavisca.Bootcamp.LanguageBasics.Exercise1/Program.cs
+++ Tavisca.Bootcamp.LanguageBasics.Exercise1/Program.cs
@@ -22,8 +22,83 @@ namespace Tavisca.Bootcamp.LanguageBasics.Exercise1
 
         public static int FindDigit(string equation)
         {
-            // Add your code here.
-            throw new NotImplementedException();
+            //considering a standard equation as a*b=ans
+
+            //splitting input string at '*' and '=' sign, to get three numbers in string format
+            string[] numbers = equation.Split(new Char [] {'*', '=' } ); 
+            
+            int operand=Int32.MinValue; //variable to store the index of number containing '?', starting from 1
+
+            for(int i=0;i<numbers.Length;i++)
+            {
+                if(numbers[i].Contains('?'))
+                {
+                    operand=i+1;
+                }
+            }
+            //if '?' is in first number
+            if(operand==1)
+            {
+                //convert other two numbers into integers
+                int b=Int32.Parse(numbers[1]);
+                int ans=Int32.Parse(numbers[2]);
+
+                //checking if b divides ans completely and does not gives out a floating point number
+                if(ans%b==0)
+                {
+                    int a=ans/b;
+                    string convertedA=a.ToString();
+                    if(convertedA.Length==numbers[0].Length) //checking for number with leading zero
+                    {
+                        int index=numbers[0].IndexOf('?');
+                        return convertedA[index]-'0';  
+                    }
+                    else{
+                        return -1;
+                    }
+                }
+                else
+                {
+                    return -1;
+                }
+            }
+            else if(operand==2) //if '?' is in b
+            {
+                int a=Int32.Parse(numbers[0]);
+                int ans=Int32.Parse(numbers[2]);
+                if(ans%a==0)
+                {
+                    int b=ans/a;
+                    string convertedB=b.ToString();
+                    if(convertedB.Length==numbers[1].Length)  //checking for number with leading zero
+                    {
+                        int index=numbers[1].IndexOf('?');
+                        return convertedB[index]-'0';
+                    }
+                    else{
+                        return -1;
+                    }
+                    
+                }
+                else
+                {
+                    return -1;
+                }
+            }
+            else //if '?' is in ans
+            {
+                int a=Int32.Parse(numbers[0]);
+                int b=Int32.Parse(numbers[1]);
+
+                int ans=a*b;
+
+                string convertedAns=ans.ToString();
+
+                int index=numbers[2].IndexOf('?');
+                
+                return convertedAns[index]-'0';
+            }
+            
         }
     }
 }
